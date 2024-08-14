e1424752db56ba472c4d9b14d2f3e9fc11ed2a69
Marianna Antonian <mariannahaney@gmail.com>
2024-08-14 12:59:34 -0400

    added base classes

---
+++ src/main/java/core/TestContext.java
@@ -0,0 +1,83 @@
+package core;
+
+import io.cucumber.java.Scenario;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.openqa.selenium.WebDriver;
+import pages.*;
+import utils.BrowserUtils;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestContext {
+
+    private final UI UI;
+    private final API API;
+    public Map<String, Object> sharedData;
+    public Exception e;
+    public Scenario scenario;
+
+    public TestContext() {
+        UI = new UI();
+        API = new API(this);
+        sharedData = new HashMap<>();
+        e = new Exception();
+    }
+    public UI UI() {
+        return this.UI;
+    }
+    public API API() {
+        return this.API;
+    }
+
+    public class UI {
+        private final WebDriver driver;
+        private final BrowserUtils browserUtils;
+        private final LoginPage loginPage;
+        private final HomePage homePage;
+        private final OppPage oppPage;
+        private final SetupPage setupPage;
+        private final AccountPage accountPage;
+
+        public UI(){
+            this.driver = new Driver().initializeDriver("chrome");
+            this.browserUtils = new BrowserUtils(driver);
+            this.loginPage = new LoginPage(driver);
+            this.homePage = new HomePage(driver);
+            this.accountPage = new AccountPage(driver);
+            this.oppPage = new OppPage(driver);
+            this.setupPage = new SetupPage(driver);
+
+        }
+
+        public WebDriver getDriver() {
+            return this.driver;
+        }
+
+        public BrowserUtils getBrowserUtils() {
+            return this.browserUtils;
+        }
+
+        public LoginPage getLoginPage() {
+            return this.loginPage;
+        }
+
+        public HomePage getHomePage() {
+            return this.homePage;
+        }
+
+        public AccountPage getAccountPage(){return this.accountPage;}
+        public OppPage getOppPage(){return this.oppPage;}
+        public SetupPage setupPage(){return this.setupPage;}
+    }
+
+    public class API {
+        public RequestSpecification requestSpecification;
+        public Response response;
+        public ApiUtils ApiUtils;
+
+        public API(TestContext testContext) {
+            ApiUtils = new ApiUtils(testContext);
+        }
+    }
+}
---
+++ src/main/java/utils/ConfigReader.java
@@ -0,0 +1,46 @@
+package utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReader {
+    public static String readProperty(String property) {
+        return switch (property) {
+            case "username-student" ->
+                    System.getenv("STUDENT_USR") != null ? System.getenv("STUDENT_USR") : new ConfigReader().getPropertyFromFile(property);
+            case "password-student" ->
+                    System.getenv("STUDENT_PSW") != null ? System.getenv("STUDENT_PSW") : new ConfigReader().getPropertyFromFile(property);
+            case "username-admin" ->
+                    System.getenv("ADMIN_USR") != null ? System.getenv("ADMIN_USR") : new ConfigReader().getPropertyFromFile(property);
+            case "password-admin" ->
+                    System.getenv("ADMIN_PSW") != null ? System.getenv("ADMIN_PSW") : new ConfigReader().getPropertyFromFile(property);
+            default -> new ConfigReader().getPropertyFromFile(property);
+        };
+    }
+
+    private String getPropertyFromFile(String property) {
+        Properties prop;
+        FileInputStream fileInput;
+
+        try {
+            fileInput = new FileInputStream("config.properties");
+            prop = new Properties();
+            prop.load(fileInput);
+
+            if (prop.getProperty(property) == null){
+                try {
+                    fileInput = new FileInputStream("config-local.properties");
+                    prop = new Properties();
+                    prop.load(fileInput);
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return prop.getProperty(property);
+    }
+}
