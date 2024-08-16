4a10d67da22894436f38e3db728704a0fb83cec2
Marianna Antonian <mariannahaney@gmail.com>
2024-08-16 15:45:31 -0400

    US-10 and US-11

--- src/main/java/stepDefs/uiSteps/LoginSteps.java
+++ src/main/java/stepDefs/uiSteps/LoginSteps.java
@@ -7,7 +7,4 @@ public class LoginSteps {
 
     private final TestContext testContext;
     public LoginSteps(TestContext testContext) {this.testContext = testContext;}
-
-    @Given("I login to salesforce app")
-    public void i_login_to_salesforce_app() {testContext.UI().getLoginPage().loginAsAdmin();}
 }
