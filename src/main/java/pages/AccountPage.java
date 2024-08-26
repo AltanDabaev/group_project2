153081d443a1f5785658bb19489565fe93cce569
Marianna Antonian <mariannahaney@gmail.com>
2024-08-26 10:37:49 -0600

    resolved conflicts AccountPage

--- src/main/java/pages/AccountPage.java
+++ src/main/java/pages/AccountPage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
 
 
 public class AccountPage {
@@ -13,6 +14,39 @@ public class AccountPage {
 
     public AccountPage(WebDriver driver){
         PageFactory.initElements(driver, this);}
+
+    public String newlyCreatedAccountName;
+
+    @FindBy(xpath = "(//p[@title='Phone']/following-sibling::p)[1]")
+    public WebElement headerPhone_text;
+    @FindBy(xpath = "(//p[@title='Website']/following-sibling::p)[1]")
+    public WebElement headerWebsite_text;
+    @FindBy(xpath = "(//p[@title='Industry']/following-sibling::p)[1]")
+    public WebElement headerIndustry_text;
+    @FindBy(xpath = "(//p[@title='Type']/following-sibling::p)[1]")
+    public WebElement headerType_text;
+    @FindBy(xpath = "(//p[@title='Account Owner']/following-sibling::p)[1]")
+    public WebElement headerAccountOwner_text;
+    @FindBy(xpath = "//th//a[@title]")
+    public List<WebElement> items_recentlyViewed;
+    @FindBy(xpath = "//a[@data-tab-value='detailTab']")
+    public List<WebElement> editDetailsBtn;
+    @FindBy(xpath = "//button[@title='Edit Account Name']")
+    public WebElement editAccountNameBtn;
+    @FindBy(xpath = "//button[@name='SaveEdit']")
+    public WebElement saveBtn;
+    @FindBy(xpath = "//input[@name='Name']")
+    public WebElement newAccountName;
+    @FindBy(xpath = "//lightning-formatted-text[@slot='primaryField']")
+    public WebElement accountHeaderText;
+
+    public void editAccountDetails(String accountName){
+        editAccountNameBtn.click();
+        newAccountName.clear();
+        newAccountName.sendKeys(accountName);
+
+        saveBtn.click();
+    }
 
     @FindBy(xpath = "//th//a[@data-refid = 'recordId']")
     public WebElement firstAccountOnTheList;
@@ -35,9 +69,6 @@ public class AccountPage {
     @FindBy(xpath = "//li[contains(@data-target-selection-name, 'Account.New')]")
     public WebElement createNewAccountBtn;
 
-    @FindBy(xpath = "//input[@name='Name']")
-    public WebElement newAccountName;
-
     @FindBy(xpath = "//input[@name='AccountNumber']")
     public WebElement newAccountNumber;
 
@@ -46,14 +77,6 @@ public class AccountPage {
 
     @FindBy(xpath = "//span[@title='Prospect']")
     public WebElement selectTypeOption_Prospect;
-
-    @FindBy(xpath = "//button[@name='SaveEdit']")
-    public WebElement saveBtn;
-
-    @FindBy(xpath = "//lightning-formatted-text[@slot='primaryField']")
-    public WebElement accountHeaderText;
-
-    public String newlyCreatedAccountName;
 
     public void createNewAccount(String accountName, String accountNumber, String type){
         createNewAccountBtn.click();
